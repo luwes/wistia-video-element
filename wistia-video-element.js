7ce2307b87065d89531553df090343fa6f524f1a
Wesley Luyten <me@wesleyluyten.com>
2023-04-08 15:01:59 -0500

    fix: update super-video-element v1.1.1 & simplify

--- wistia-video-element.js
+++ wistia-video-element.js
@@ -26,25 +26,13 @@ templateShadowDOM.innerHTML = /*html*/`
 
 class WistiaVideoElement extends SuperVideoElement {
   static template = templateShadowDOM;
-
-  constructor() {
-    super();
-    this.loadComplete = new PublicPromise();
-  }
+  static skipAttributes = ['src'];
 
   get nativeEl() {
     return this.api?.elem();
   }
 
   async load() {
-    if (this.hasLoaded) this.loadComplete = new PublicPromise();
-    this.hasLoaded = true;
-
-    this.dispatchEvent(new Event('loadstart'));
-
-    // Wait 1 tick to allow other attributes to be set.
-    await Promise.resolve();
-
     const MATCH_SRC = /(?:wistia\.com|wi\.st)\/(?:medias|embed)\/(.*)$/i;
     const id = this.src.match(MATCH_SRC)[1];
     const options = {
@@ -75,19 +63,9 @@ class WistiaVideoElement extends SuperVideoElement {
         options,
       });
     });
-
-    this.dispatchEvent(new Event('loadcomplete'));
-    this.loadComplete.resolve();
-
-    await this.loadComplete;
   }
 
   async attributeChangedCallback(attrName, oldValue, newValue) {
-    // This is required to come before the await for resolving loadComplete.
-    if (attrName === 'src' && newValue) {
-      this.load();
-      return;
-    }
 
     if (attrName === 'controls') {
       await this.loadComplete;
@@ -116,80 +94,29 @@ class WistiaVideoElement extends SuperVideoElement {
   play() {
     // wistia.play doesn't return a play promise.
     this.api.play();
-    return promisify(this.addEventListener.bind(this))('playing');
-  }
-
-  // If the getter from SuperVideoElement is overriden, it's required to define
-  // the setter again too unless it's a readonly property! It's a JS thing.
-
-  get src() {
-    return this.getAttribute('src');
-  }
-
-  set src(val) {
-    this.setAttribute('src', val);
-  }
-
-  get controls() {
-    return this.hasAttribute('controls');
-  }
-
-  set controls(val) {
-    this.toggleAttribute('controls', Boolean(val));
+    return new Promise((resolve) => this.addEventListener('playing', resolve));
   }
 }
 
 const loadScriptCache = {};
-async function loadScript(src, globalName, readyFnName) {
+async function loadScript(src, globalName) {
+  if (!globalName) return import(src);
   if (loadScriptCache[src]) return loadScriptCache[src];
-  if (globalName && self[globalName]) {
-    await delay(0);
-    return self[globalName];
-  }
-  return (loadScriptCache[src] = new Promise(function (resolve, reject) {
+  if (self[globalName]) return self[globalName];
+  return (loadScriptCache[src] = new Promise((resolve, reject) => {
     const script = document.createElement('script');
+    script.defer = true;
     script.src = src;
-    const ready = () => resolve(self[globalName]);
-    if (readyFnName) (self[readyFnName] = ready);
-    script.onload = () => !readyFnName && ready();
+    script.onload = () => resolve(self[globalName]);
     script.onerror = reject;
     document.head.append(script);
   }));
-}
-
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-export function promisify(fn) {
-  return (...args) =>
-    new Promise((resolve) => {
-      fn(...args, (...res) => {
-        if (res.length > 1) resolve(res);
-        else resolve(res[0]);
-      });
-    });
 }
 
 let idCounter = 0;
 export function uniqueId(prefix) {
   const id = ++idCounter;
   return `${prefix}${id}`;
-}
-
-/**
- * A utility to create Promises with convenient public resolve and reject methods.
- * @return {Promise}
- */
-class PublicPromise extends Promise {
-  constructor(executor = () => {}) {
-    let res, rej;
-    super((resolve, reject) => {
-      executor(resolve, reject);
-      res = resolve;
-      rej = reject;
-    });
-    this.resolve = res;
-    this.reject = rej;
-  }
 }
 
 if (!globalThis.customElements.get('wistia-video')) {
